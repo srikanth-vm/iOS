169fa6f4150eebf71d635de4c35d4531e7688ef8
srikanth-vm <vm.shrikanth@gmail.com>
2014-04-02 16:59:22 +0530

    AVPlayerView Example Application

---
+++ AVPlayer/AVPlayer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  AVPlayer
+//
+//  Created by Srikanth on 02/04/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
